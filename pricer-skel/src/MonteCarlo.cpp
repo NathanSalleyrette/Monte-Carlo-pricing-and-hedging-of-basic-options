ac7b8603748d97dc1d38a198e9f318f518c16608
Loic Simeon <Loic.Simeon@grenoble-inp.org>
2021-09-21 09:18:43 +0200

    premiere version de profit and loss

--- pricer-skel/src/MonteCarlo.cpp
+++ pricer-skel/src/MonteCarlo.cpp
@@ -54,7 +54,7 @@ void MonteCarlo::price(double &prix, double &std_dev){
 
 
 void MonteCarlo::price(const PnlMat *past, double t, double &prix, double &std_dev) {
-    double sumpayoff = 0.0;
+    double price = 0.0;
     double squaresum = 0.0;
     double respayoff = 0.0;
     double sigma = 0.0;
@@ -67,7 +67,7 @@ void MonteCarlo::price(const PnlMat *past, double t, double &prix, double &std_d
         this->mod_->asset(path, t, this->opt_->T_ , this->opt_->nbTimeSteps_, this->rng_, past);
         
         respayoff = this->opt_->payoff(path);
-        sumpayoff += respayoff;
+        price += respayoff;
         squaresum += respayoff * respayoff;
 
         PnlMat *shiftpath = pnl_mat_copy(path);
@@ -89,8 +89,8 @@ void MonteCarlo::price(const PnlMat *past, double t, double &prix, double &std_d
         }
     }
 
-    prix =  sumpayoff*exp(- this->mod_->r_ * (this->opt_->T_ - t)) / this->nbSamples_;
-    sigma = squaresum / this->nbSamples_ - (sumpayoff / this->nbSamples_)*(sumpayoff / this->nbSamples_);
+    prix =  price*exp(- this->mod_->r_ * (this->opt_->T_ - t)) / this->nbSamples_;
+    sigma = squaresum / this->nbSamples_ - (price / this->nbSamples_)*(price / this->nbSamples_);
     sigma = sigma*exp(- 2 * this->mod_->r_ * (this->opt_->T_ - t));
     std_dev = sqrt(sigma) / sqrt(this->nbSamples_);
 }
@@ -144,7 +144,7 @@ void MonteCarlo :: profitAndLoss(PnlVect *descriptionTime, PnlMat *pathReal, dou
     
     double prix = 0.0;
     double std_dev = 0.0;
-
+    double step = this->opt_->T_/this->opt_->nbTimeSteps_;
     double value = 0.0;
     PnlVect *deltaNow = pnl_vect_create(pathReal->n);
     PnlVect *deltaLast = pnl_vect_create(pathReal->n);
@@ -157,12 +157,12 @@ void MonteCarlo :: profitAndLoss(PnlVect *descriptionTime, PnlMat *pathReal, dou
 
     PnlMat *past = pnl_mat_create(pathReal->n, this->opt_->nbTimeSteps_);
 
-    double step = this->opt_->T_/ this->opt_->nbTimeSteps_;
-    
-    for(int i = 0; i < this->opt_->nbTimeSteps_; i++){
-        //Creation de la matrice Past
-        
+    for(int i = 0; i < this->opt_->nbTimeSteps_; i++ ){
+        pnl_mat_get_row(lignSpots, pathReal, (int)(i*step));
+        pnl_mat_set_row(past, lignSpots,i);
     }
+
+    
 
     for(int i = 0 ; i <descriptionTime->size;  i++){
         // Calcul des différentes valeurs de portefeuille
